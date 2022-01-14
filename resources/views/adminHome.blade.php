b5feb19e13a54146239fdb484be5747388d12bd7
Cassio Matoso <cassio.matoso@sgtgestaoetecnologia.com.br>
2022-01-14 14:25:03 -0300

    Adicionando boas-vindas com o nome do usuário

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Olá, ' . Auth::user()->name . '!') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('Logado como Admin!') }}
+                    
                     
                 </div>
             </div>
