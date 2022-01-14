6bb36fcb4e4e6b19045406a14e3ebec033443714
cassiomatoso <cassiocg98@gmail.com>
2022-01-13 22:11:10 -0300

    fix "Method App\Http\Controllers\HomeController::adminHome does not exist."

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,9 +31,9 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function homeAdmin()
+    public function adminHome()
     {
-        return view('homeAdmin');
+        return view('adminHome');
     }
 
     
