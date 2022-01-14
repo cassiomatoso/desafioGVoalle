8eee8fcc3ff6a79a75d91b5ece2902e7e2dfec64
cassiomatoso <cassiocg98@gmail.com>
2022-01-13 21:23:56 -0300

    Comentário

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Show the application client-dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
@@ -27,7 +27,7 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Show the application admin-dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
