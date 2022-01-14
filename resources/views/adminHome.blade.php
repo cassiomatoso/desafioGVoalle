10f9ff53b2b0ac285a7fc90d2d89dbd0a49b6b2e
Cassio Matoso <cassio.matoso@sgtgestaoetecnologia.com.br>
2022-01-14 18:04:07 -0300

    Iniciando front-end

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -1,24 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Olá, ' . Auth::user()->name . '!') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Olá, ' . Auth::user()->name . '!') }}
+                    </div>
+                    <div class="card-body">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Produtos</h5>
+                                        <p class="card-text">Módulo para o gerenciamento dos produtos.</p>
+                                        <a href="#" class="btn btn-primary">Acessar</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Clientes</h5>
+                                        <p class="card-text">Módulo para o gerenciamento dos Clientes</p>
+                                        <a href="#" class="btn btn-primary">Acessar</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    @endif
-
-                    
-                    
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Pedidos</h5>
+                                        <p class="card-text">Controle dos pedidos.</p>
+                                        <a href="#" class="btn btn-primary">Acessar</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Usuários</h5>
+                                        <p class="card-text">Gerenciamento dos usuários</p>
+                                        <a href="#" class="btn btn-primary">Acessar</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,10 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Grupo Voalle') }}</title>
+    
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('storage/images/favicon.png') }}" type="image/x-icon" />
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -24,7 +27,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', '') }}
+                    {{ config('app.name', 'Grupo Voalle') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
