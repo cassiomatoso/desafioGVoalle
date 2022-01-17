6425af508866d9a98bacdfbbb8981603cb0d9a63
Cassio Matoso <cassio.matoso@sgtgestaoetecnologia.com.br>
2022-01-17 19:16:16 -0300

    Padronizando plural nos seeders

--- database/seeders/CreateCustomersSeeder.php
+++ database/seeders/CreateCustomersSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Customer;
 
-class CreateCustomerSeeder extends Seeder
+class CreateCustomersSeeder extends Seeder
 {
     /**
      * Run the database seeds.
