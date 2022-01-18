e3aff81dd6cb9c994de7d99075240737f5f308e9
Cassio Matoso <cassio.matoso@sgtgestaoetecnologia.com.br>
2022-01-18 16:35:08 -0300

    view de edição

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,65 @@
+@extends('products.layout')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="pull-left">
+                            <h3> Edição - Produto </h3>
+                        </div>
+                        <div class="pull-right">
+                            <a class="btn btn-primary" href="{{ route('products.index') }}"> Voltar</a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                        </div>
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <strong>Error!</strong> Houve algum problema.<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <form action="{{ route('products.update', $product->id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <strong>Produto:</strong>
+                                        <input type="text" name="title" class="form-control"
+                                            value="{{ $product->title }}" placeholder="Ex.: ERPVoalle">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <strong>R$</strong>
+                                        <input type="number" name="price" class="form-control"
+                                            value="{{ $product->price }}" placeholder="Apenas números">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <strong>Descrição:</strong>
+                                        <textarea class="form-control" name="description"
+                                            placeholder="Ex.: Sistema gerencial para empresas">{{ $product->description }}</textarea>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
