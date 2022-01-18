076b706dc628efd74aa2f8fe636f6a35d49732fb
cassiomatoso <cassiocg98@gmail.com>
2022-01-18 19:16:27 -0300

    Atualizando ProductsSeeder para mostrar paginação

--- database/seeders/CreateProductsSeeder.php
+++ database/seeders/CreateProductsSeeder.php
@@ -24,6 +24,41 @@ class CreateProductsSeeder extends Seeder
                 'title'=>'VoallePay',
                 'description'=>'Plataforma de pagamento para gestão de recebíveis. Simples, ágil e segura.',
                 'price'=>'5000',
+            ],
+            [
+                'title'=>'VoalleAcademy',
+                'description'=>'Plataforma de cursos para estudantes de tecnologia.',
+                'price'=>'15000',
+            ],
+            [
+                'title'=>'VoalleParking',
+                'description'=>'Sistema para administração de estacionamento rotativo',
+                'price'=>'2000',
+            ],
+            [
+                'title'=>'VoalleGym',
+                'description'=>'Plataforma com informações sobre saúde para os colaboradores do Grupo Voalle',
+                'price'=>'2000',
+            ],
+            [
+                'title'=>'BlogVoalle',
+                'description'=>'Blog completo com hospedagem gratuita.',
+                'price'=>'1000',
+            ],
+            [
+                'title'=>'VoalleHelp',
+                'description'=>'Plataforma focada em atender demandas e organizar chamados para TI.',
+                'price'=>'3000',
+            ],
+            [
+                'title'=>'CMSVoalle',
+                'description'=>'Sistema de Gestão de Conteúdos. Mais que um blog.',
+                'price'=>'11000',
+            ],
+            [
+                'title'=>'VoalleKids',
+                'description'=>'Plataforma gameficada com foco no desenvolvimento infantil',
+                'price'=>'12000',
             ]
         ];
 
