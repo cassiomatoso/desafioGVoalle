b2c7b86a09f046a0f95dd0e4aca80238a87ba7a4
cassiomatoso <cassiocg98@gmail.com>
2022-01-18 20:29:03 -0300

    Update cliente

---
+++ resources/views/customers/edit.blade.php
@@ -0,0 +1,70 @@
+@extends('customers.layout')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="pull-left">
+                            <h3> Edição - Cliente </h3>
+                        </div>
+                        <div class="pull-right">
+                            <a class="btn btn-primary" href="{{ route('customers.index') }}"> Voltar</a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                        </div>
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <strong>Error!</strong> Houve algum problema.<br><br>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <form action="{{ route('customers.update', $customer->id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <strong>Razão Social:</strong>
+                                        <input type="text" name="company" value="{{ $customer->company }}" class="form-control" placeholder="Ex.: Grupo Voalle">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <strong>E-Mail principal:</strong>
+                                        <input type="email" name="email" value="{{ $customer->email }}" class="form-control" placeholder="Ex.: razaosocial@dominio.com.br">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <strong>CNPJ:</strong>
+                                        <input type="text" name="cnpj" value="{{ $customer->cnpj }}" class="form-control" placeholder="Ex.: 00.000.000/0001-00">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <strong>Endereço:</strong>
+                                        <input type="text" name="address" value="{{ $customer->address }}" class="form-control" placeholder="Ex.: BR-158, 204 – Bairro Urlândia | Santa Maria/RS">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
