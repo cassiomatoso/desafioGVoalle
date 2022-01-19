3ededb3e243a6664a96572154f2febe84d421c75
cassiomatoso <cassiocg98@gmail.com>
2022-01-18 23:30:33 -0300

    Correção de relacionamento

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -17,4 +17,16 @@ class Purchase extends Model
     protected $fillable = [
        'amount', 'created_at', 'customer_id', 'product_id', 'user_id'
     ];
+
+    public function product(){
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function customer(){
+        return $this->belongsTo(Customer::class, 'customer_id');
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
